eab2bddf4a60fa5b156f431f45eb3bf608711e88
Anjali-sonawane <64551835+Anjali-sonawane@users.noreply.github.com>
2020-08-16 12:18:40 +0530

    AWP EXAM

---
+++ 200240520018_Anjali Sonawane/2.js
@@ -0,0 +1,13 @@
+
+
+function submit()
+{
+    const username = document.getElementById("U1").value;
+    const password=document.getElementById("p1").value;
+    const emailid=document.getElementById("E1").value;
+
+    alert("USERNAME:"+ username);
+    alert("PASSWORD:"+ password);
+    alert("EMAIL_ID IS:"+ emailid);
+};
+
